eaef0532e09c5b28990c47545d38360ead533711
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-09-15 12:26:29 -0300

    Adding get pokemonByIdOrName function to pokeApi

--- src/services/pokeApi.js
+++ src/services/pokeApi.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const pokeApi = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2'
-})
+const baseURL ='https://pokeapi.co/api/v2';
 
-export default pokeApi;
+async function getPokemonByIdOrName(typedPokemon) {
+    return await axios.get(`${baseURL}/pokemon/${typedPokemon}`)
+                        .then(response => response.data)
+                        .catch(_ => 'Pokemon não encontrado')
+}
+
+export { getPokemonByIdOrName };
--- src/services/pokeApi.test.js
+++ src/services/pokeApi.test.js
@@ -1,7 +1,30 @@
-import pokeApi from "./pokeApi";
+import { getPokemonByIdOrName } from "./pokeApi";
+import axios from "axios";
 
-describe('PokeApi', () => {
-    it('should not be undefined', () => {
-        expect(pokeApi).not.toBeUndefined()
+
+describe('Get Pokemon By ID Or Name', () => {
+    jest.mock('axios')
+    axios.get = jest.fn()
+
+    it('shoul return pokemon data when pokemon exists', async () => {
+        let fakeResponse = { data: [{ "abilities": [{ "ability": { "name": "overgrow", "url": "https://pokeapi.co/api/v2/ability/65/" }, "is_hidden": false, "slot": 1 }, { "ability": { "name": "chlorophyll", "url": "https://pokeapi.co/api/v2/ability/34/" }, "is_hidden": true, "slot": 3 }] }] }
+        axios.get.mockResolvedValue(fakeResponse)
+
+        let response = await getPokemonByIdOrName('pikachu')
+
+        expect(axios.get).toBeCalled()
+        expect(response).toBe(fakeResponse.data)
     })
+
+    it('should return error message when pokemon is not found', async () => {
+        axios.get.mockImplementationOnce(() =>
+            Promise.reject(),
+        );
+
+        let response = await getPokemonByIdOrName('test')
+
+        expect(axios.get).toBeCalled()
+        expect(response).toBe('Pokemon não encontrado')
+    })
+
 })
