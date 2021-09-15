a9e5e0f8f9d34efc702706ece3a330600e595807
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-09-14 23:26:00 -0300

    Adding textbox and submit button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { useState } from "react"
-import './App.css';
 
 const App = () => {
+
+  const [typedPokemon, setTypedPokemon] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+
+function handleChange(event){
+  setTypedPokemon(event.target.value)
+}
+
+function handleSubmit(event){
+  event.preventDefault()
+  if(!typedPokemon){
+    return
+  }
+  setIsLoading(true)
+}
+
   return (
     <div>
       <h1>Bem vindo à pokedex!</h1>
       <p>Digite o nome ou a ID do pokemon!</p>
+      <form onSubmit={handleSubmit}>
+        <input value={typedPokemon} 
+               placeholder={"Nome ou ID do pokemon"} 
+               onChange={handleChange}
+        ></input>
+        <button type="submit">
+          {isLoading ? (<span>Carregando....</span>) : (<>Buscar</>)}
+        </button>
+      </form>
     </div>
   );
 }
+
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,48 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
 describe('App', () => {
-  it('should render welcome ', () => {
-    render(<App/>);
+  it('should render page elements ', () => {
+    render(<App />);
 
     const welcomeText = screen.getByRole('heading')
     const paragraph = screen.getByText('Digite o nome ou a ID do pokemon!')
+    const input = screen.getByRole('textbox')
+    const submitButton = screen.getByRole('button')
 
     expect(welcomeText).toBeInTheDocument()
     expect(paragraph).toBeInTheDocument()
+    expect(input).toBeInTheDocument()
+    expect(submitButton).toBeInTheDocument()
+  })
+
+  it('should render Buscar when page is not loading', () => {
+    render(<App />);
+
+    const buscarText = screen.getByText('Buscar')
+
+    expect(buscarText).toBeInTheDocument()
+  })
+
+  it('should do noting when there is no typed pokemon', () => {
+    render(<App />);
+    userEvent.click(screen.getByRole('button'))
+
+    const buscarText = screen.getByText('Buscar')
+
+    expect(buscarText).toBeInTheDocument()
+  })
+
+
+  it('should render Carregando.... when page is loading', () => {
+    render(<App />);
+    userEvent.type(screen.getByRole('textbox'), 'pikachu')
+    userEvent.click(screen.getByRole('button'))
+
+    const buscarText = screen.getByText('Carregando....')
+    expect(buscarText).toBeInTheDocument()
   })
 
 })
