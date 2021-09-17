9d274e1f3d807181ccb4e629a905d657fe90f9f7
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-09-16 23:16:38 -0300

    Adding pokemon render and not found

--- src/App.js
+++ src/App.js
@@ -1,39 +1,89 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react";
+import { getPokemonByIdOrName } from "./services/pokeApi";
 
 const App = () => {
+  const [pokemon, setPokemon] = useState(null);
+  const [error, setError] = useState(null);
+  const [typedPokemon, setTypedPokemon] = useState("");
 
-  const [typedPokemon, setTypedPokemon] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+  useEffect(async () => {
+    await changeStates();
+  }, [error]);
 
-
-function handleChange(event){
-  setTypedPokemon(event.target.value)
-}
-
-function handleSubmit(event){
-  event.preventDefault()
-  if(!typedPokemon){
-    return
+  function handleChange(event) {
+    setTypedPokemon(event.target.value);
   }
-  setIsLoading(true)
-}
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    changeStates();
+  }
+
+  async function changeStates() {
+    if (!typedPokemon) {
+      return;
+    }
+
+    let pokemon = await getPokemonByIdOrName(typedPokemon);
+
+    if (typeof pokemon === "string") {
+      setError(pokemon);
+      setPokemon(null);
+    } else {
+      setPokemon(pokemon);
+      setError(null);
+    }
+  }
+
+  function renderPokemonCard(pokemon) {
+    return (
+      <div key={pokemon.id}>
+        <>
+          <div>
+            <h2>{pokemon.name}</h2>
+            <img
+              src={
+                pokemon.sprites.versions["generation-v"]["black-white"].animated
+                  .front_default
+              }
+              alt={pokemon.name}
+            />
+          </div>
+          <div>
+            <span>
+              <strong>Height</strong>: {pokemon.height * 10} cm
+            </span>
+            <span>
+              <strong>Weight</strong>: {pokemon.weight / 10} kg
+            </span>
+            <span>
+              <strong>Type</strong>: {pokemon.types[0].type.name}
+            </span>
+            <span>
+              <strong>id</strong>: {pokemon.id}
+            </span>
+          </div>
+        </>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h1>Bem vindo à pokedex!</h1>
       <p>Digite o nome ou a ID do pokemon!</p>
       <form onSubmit={handleSubmit}>
-        <input value={typedPokemon} 
-               placeholder={"Nome ou ID do pokemon"} 
-               onChange={handleChange}
+        <input
+          value={typedPokemon}
+          placeholder={"Nome ou ID do pokemon"}
+          onChange={handleChange}
         ></input>
-        <button type="submit">
-          {isLoading ? (<span>Carregando....</span>) : (<>Buscar</>)}
-        </button>
+        <button type="submit">Buscar</button>
       </form>
+      {error && <span>{error}</span>}
+      {pokemon && renderPokemonCard(pokemon)}
     </div>
   );
-}
-
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,48 +1,77 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
-import { act } from 'react-dom/test-utils';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+import { getPokemonByIdOrName } from "./services/pokeApi";
+import { act } from "react-dom/test-utils";
 
-describe('App', () => {
-  it('should render page elements ', () => {
+jest.mock("./services/pokeApi");
+
+describe("App", () => {
+  it("should render page elements ", () => {
     render(<App />);
 
-    const welcomeText = screen.getByRole('heading')
-    const paragraph = screen.getByText('Digite o nome ou a ID do pokemon!')
-    const input = screen.getByRole('textbox')
-    const submitButton = screen.getByRole('button')
+    const welcomeText = screen.getByRole("heading");
+    const paragraph = screen.getByText("Digite o nome ou a ID do pokemon!");
+    const input = screen.getByRole("textbox");
+    const submitButton = screen.getByRole("button");
 
-    expect(welcomeText).toBeInTheDocument()
-    expect(paragraph).toBeInTheDocument()
-    expect(input).toBeInTheDocument()
-    expect(submitButton).toBeInTheDocument()
-  })
+    expect(welcomeText).toBeInTheDocument();
+    expect(paragraph).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
 
-  it('should render Buscar when page is not loading', () => {
+  it("should render Buscar when page is not loading", () => {
     render(<App />);
 
-    const buscarText = screen.getByText('Buscar')
+    const buscarText = screen.getByText("Buscar");
 
-    expect(buscarText).toBeInTheDocument()
-  })
+    expect(buscarText).toBeInTheDocument();
+  });
 
-  it('should do noting when there is no typed pokemon', () => {
+  it("should do noting when there is no typed pokemon", () => {
     render(<App />);
-    userEvent.click(screen.getByRole('button'))
+    userEvent.click(screen.getByRole("button"));
 
-    const buscarText = screen.getByText('Buscar')
+    const buscarText = screen.getByText("Buscar");
 
-    expect(buscarText).toBeInTheDocument()
-  })
+    expect(buscarText).toBeInTheDocument();
+  });
 
-
-  it('should render Carregando.... when page is loading', () => {
+  it("should setError and stop loading when theres no pokemon", async () => {
+    getPokemonByIdOrName.mockResolvedValue("Pokemon não encontrado.");
     render(<App />);
-    userEvent.type(screen.getByRole('textbox'), 'pikachu')
-    userEvent.click(screen.getByRole('button'))
 
-    const buscarText = screen.getByText('Carregando....')
-    expect(buscarText).toBeInTheDocument()
-  })
+    userEvent.type(screen.getByRole("textbox"), "test");
 
-})
+    await act(async () => {
+      userEvent.click(screen.getByRole("button"));
+    });
+
+    const errorSpan = screen.getByText("Pokemon não encontrado.");
+    expect(errorSpan).toBeInTheDocument();
+  });
+
+  it("should show pokemon when theres a pokemon", async () => {
+    var json = {
+      id: 1,
+      name: "pikachu",
+      height: 4,
+      weight: 3,
+      types: [{ type: { name: "eletric" } }],
+      sprites: {
+        versions: { "generation-v": { "black-white": { animated: "bla" } } },
+      },
+    };
+    getPokemonByIdOrName.mockResolvedValue(json);
+    render(<App />);
+
+    userEvent.type(screen.getByRole("textbox"), "pikachu");
+
+    await act(async () => {
+      userEvent.click(screen.getByRole("button"));
+    });
+    const pikachuSpan = screen.queryByText("pikachu");
+    expect(pikachuSpan).toBeInTheDocument();
+  });
+});
