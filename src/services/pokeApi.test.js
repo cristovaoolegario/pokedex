70057c326045646bc8d898f4554fdfaa4fcb7ede
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-09-18 00:00:06 -0300

    Format pokeApi.test.js file

--- src/services/pokeApi.test.js
+++ src/services/pokeApi.test.js
@@ -1,30 +1,48 @@
 import { getPokemonByIdOrName } from "./pokeApi";
 import axios from "axios";
 
+describe("Get Pokemon By ID Or Name", () => {
+  jest.mock("axios");
+  axios.get = jest.fn();
 
-describe('Get Pokemon By ID Or Name', () => {
-    jest.mock('axios')
-    axios.get = jest.fn()
+  it("shoul return pokemon data when pokemon exists", async () => {
+    let fakeResponse = {
+      data: [
+        {
+          id: 1,
+          name: "pikachu",
+          height: 4,
+          weight: 3,
+          types: [{ type: { name: "eletric" } }],
+          sprites: {
+            versions: {
+              "generation-v": {
+                "black-white": {
+                  front_default: "htttp://test.com/static-img.png",
+                  animated: {
+                    front_default: "http://test.com/animated-img.gif",
+                  },
+                },
+              },
+            },
+          },
+        },
+      ],
+    };
+    axios.get.mockResolvedValue(fakeResponse);
 
-    it('shoul return pokemon data when pokemon exists', async () => {
-        let fakeResponse = { data: [{ "abilities": [{ "ability": { "name": "overgrow", "url": "https://pokeapi.co/api/v2/ability/65/" }, "is_hidden": false, "slot": 1 }, { "ability": { "name": "chlorophyll", "url": "https://pokeapi.co/api/v2/ability/34/" }, "is_hidden": true, "slot": 3 }] }] }
-        axios.get.mockResolvedValue(fakeResponse)
+    let response = await getPokemonByIdOrName("pikachu");
 
-        let response = await getPokemonByIdOrName('pikachu')
+    expect(axios.get).toBeCalled();
+    expect(response).toBe(fakeResponse.data);
+  });
 
-        expect(axios.get).toBeCalled()
-        expect(response).toBe(fakeResponse.data)
-    })
+  it("should return error message when pokemon is not found", async () => {
+    axios.get.mockImplementationOnce(() => Promise.reject());
 
-    it('should return error message when pokemon is not found', async () => {
-        axios.get.mockImplementationOnce(() =>
-            Promise.reject(),
-        );
+    let response = await getPokemonByIdOrName("test");
 
-        let response = await getPokemonByIdOrName('test')
-
-        expect(axios.get).toBeCalled()
-        expect(response).toBe('Pokemon não encontrado')
-    })
-
-})
+    expect(axios.get).toBeCalled();
+    expect(response).toBe("Pokemon não encontrado");
+  });
+});
