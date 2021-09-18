8a6248b891b6ffe4e8fd70674739d766d546f8b9
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-09-18 18:29:51 -0300

    Adding language detector based on path and browser configuration

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,20 +1,31 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import XHR from "i18next-http-backend";
 
-i18n.use(initReactI18next).init({
-  fallbackLng: "en",
-  lng: "en",
-  resources: {
-    en: {
-      translations: require("./locales/en/translations.json"),
+const options = {
+  order: ["querystring", "navigator"],
+  lookupQuerystring: "lng",
+};
+
+i18n
+  .use(XHR)
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    detection: options,
+    resources: {
+      en: {
+        translations: require("./locales/en/translations.json"),
+      },
+      "pt-BR": {
+        translations: require("./locales/pt-BR/translations.json"),
+      },
     },
-    "pt-BR": {
-      translations: require("./locales/pt-BR/translations.json"),
-    },
-  },
-  ns: ["translations"],
-  defaultNS: "translations",
-});
+    ns: ["translations"],
+    defaultNS: "translations",
+  });
 
 i18n.languages = ["en", "pt-BR"];
 
